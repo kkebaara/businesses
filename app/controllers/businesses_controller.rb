000f226826203e25aeda5b5a9685258d290c4cbb
Kenneth Kebaara <kkebaara@yahoo.com>
2022-04-13 21:41:44 -0600

    added model relationships

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,21 @@
 class BusinessesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    def index 
+        businesses = Business.all 
+        render json: businesses 
+    end
+
+
+    def show
+        business = Business.find(params[:id])
+        render json: business
+    end
+
+    private
+
+    def render_not_found
+        render json: {"error": "Business not found"}, status: :not_found
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :favorites, dependent: destroy
+    has_many :businesses, through: favorites 
 end
